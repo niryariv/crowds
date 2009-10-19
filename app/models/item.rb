66784a921a6b9d62f022d4bee0259ea29a7d3f81
Nir Yariv <niryariv@gmail.com>
2009-10-19 10:38:36 -0400

    integrated URL normalizing into Item, deprecating Trueurl model

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,32 +4,23 @@ class Item < ActiveRecord::Base
   
   validates_uniqueness_of :url, :scope => :feed_id
   
-  before_save :get_title, :verify_url, :verify_unique
+  before_validation :normalize_it
   
-
-  # This is slow and wasteful. But, since now it's only used for post URLs, it's not that big a deal.
-  def verify_unique
-    Item.find(:first, :conditions=>['feed_id=? AND url=?', self.feed_id, self.url]).nil?
+  
+  def normalize_it
+    u = open(self.url)
+    self.url = u.base_uri.to_s  # get real, unshortened, URL
+    self.title = extract_title(u.read) if self.title.nil? # get title - unless the URL is a feed post URL in which we already have it
   end
-  
-  def get_title
-    if self.title.nil?
-      html = open(self.url).read
-      self.title = html.scan(/<title.*?>(.*?)<\/title>/mi).first.to_s.strip
-    end
+
+
+  def extract_title(html)
+    html.scan(/<title.*?>(.*?)<\/title>/mi).first.to_s.strip
   rescue Exception=>e
-    logger.error "Item.get_title (#{self.url}) Failed: #{e.message}"
-    self.title = '' if self.title.nil?
+    ''
   end
-
-
-  # Handles URLs that redirect. Ruby open() handles redirection automatically, but there's no way to get the real URL from it
-  # so this method is just to make sure we have the correct URLs in the DB.
-  def verify_url
-    self.url = Trueurl.get(self.url)
-  end
-
-
+  
+  
   # Housekeeping: removes old items from the DB
   def self.delete_old
     self.delete_all "created_at < '#{(Gaps.max+10).days.ago.to_s(:db)}'"
