41cc0810fa297c1dea027efe43846c08094b6191
Nir Yariv <niryariv@gmail.com>
2009-11-17 19:56:08 -0500

    cleaning up remains of auth code..

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  # map.resources :feeds
-  # 
-  # map.resources :items
-  # 
+
+  map.root :controller => :crowds, :action => :index
+
   map.resources :crowds
-  # 
-  map.resources :password_resets
 
   # The priority is based upon order of creation: first created -> highest priority.
   
@@ -15,7 +12,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :item, :belongs_to => :crowd
   
   
-  map.import '/crowd/:crowd_id/import', :controller => "feeds", :action => "create" #/:url, :url => /.*/
+  map.import '/crowd/:crowd_id/import', :controller => "feeds", :action => "create"
   
   # Sample of regular route:
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
@@ -25,22 +22,10 @@ ActionController::Routing::Routes.draw do |map|
   # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  map.connect '', :controller => "crowds"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
   map.connect ':controller/service.wsdl', :action => 'wsdl'
-  
-  # AuthLogic
-  map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
-
-  map.resource :account, :controller => "users"
-  map.resources :users
-
-  
 
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
