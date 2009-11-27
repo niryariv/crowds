d25c17ff23f783b40d1f68d64c21b6decd8df03b
Nir Yariv <niryariv@gmail.com>
2009-11-27 13:30:52 -0500

    spaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   
   def login_button
-    form_tag (session_url, {:style=>'text-align:bottom;'}) do
+    form_tag session_url, {:style=>'text-align:bottom;'} do
        hidden_field_tag("openid_identifier", "https://www.google.com/accounts/o8/id") +
        image_submit_tag("openid/googleW.png")
     end
