75f51fe2ed96f824b4a34ecc1f4a4b46f1e3cf37
Nir Yariv <niryariv@gmail.com>
2009-12-24 14:19:36 -0500

    correct message for user visiting feeds page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   
   def login_button
-    form_tag session_url, {:id=>'login-button'} do
+    form_tag session_url, { :id => 'login-button' } do
        hidden_field_tag("openid_identifier", "https://www.google.com/accounts/o8/id") +
        image_submit_tag("openid/googleW.png")
     end
