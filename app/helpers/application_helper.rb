e57df8cdbe12d6ea3a69c2bc66b9485a58f6d95c
Nir Yariv <niryariv@gmail.com>
2009-12-26 17:50:51 -0500

    shareThis helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,13 @@ module ApplicationHelper
        image_submit_tag("openid/googleW.png")
     end
   end
+  
+  def sharethis_button
+      '<script type="text/javascript" src="http://w.sharethis.com/button/sharethis.js#publisher=3675c913-7ace-4cb1-b80d-200793078aed&amp;type=website&amp;buttonText=Share&amp;post_services=email%2Cfacebook%2Ctwitter%2Cmyspace%2Cdigg%2Cdelicious%2Cstumbleupon%2Creddit%2Clinkedin%2Cblogger%2Cwordpress%2Cnewsvine%2Clivejournal%2Ctypepad"></script>'
+  end
 
+  def share_link(url)
+      '<script language="javascript" type="text/javascript">SHARETHIS.addEntry({title:"share"}, {button:true} );</script>'
+  end
+  
 end
