cd360c34861da127b039bd2d5da7aa2c4b5a2e70
Nir Yariv <niryariv@gmail.com>
2010-01-06 15:22:10 -0500

    handle errors better

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -72,6 +72,43 @@ class Feed < ActiveRecord::Base
 
  
   require 'feedzirra'
+
+  def feedzirra_refresh(body)
+      items = {}
+
+      rss = Feedzirra::Feed.parse(body)
+
+      rss.entries.each do |i|
+          puts "checking #{i.title}"
+          published = i.last_modified
+          next if (!self.last_read_at.nil? and published < self.last_read_at) or published < (Gaps.max+10).days.ago \
+
+          begin
+              # items[i.url] = { :created_at=>published, :title=>i.title }
+              it = self.items.create(:url=>i.url, :created_at=>published, :title=>i.title, :normalized => false)
+          rescue Exception => e # items already existed
+              puts "ERROR Feed::feedzirra_refresh #{e}"
+              next 
+          end
+          
+          i.summary.to_s.scan(/(http:\/\/.*?)[$|\'|\"|\s|\<]/i).flatten.uniq.each do |u|
+            unless u =~ /(\.mp3|\.mp4|\.mpeg|\.mpg|\.mov|\.gif|\.jpg|\.jpeg|\.png|\.js)$/i \
+              or u.include?('http://feedads.') \
+              or u.include?('http://ads.') \
+              or u.include?('http://ad.') \
+              or items.keys.include?(u)
+                begin
+                    self.items.create(:url=>u, :created_at=>published, :source_id=>it.id, :normalized => false)
+                rescue Exception => e
+                    puts "ERROR Feed::feedzirra_refresh #{e}"
+                end
+                # items[u] = {:created_at=>published, :parent=>i.url}
+            end 
+          end
+      end
+      
+      items
+  end
   
   def feedzirra_get_links(body)
       items = {}
@@ -84,7 +121,7 @@ class Feed < ActiveRecord::Base
       rss.entries.each do |i|
           puts "checking #{i.title}"
           published = i.last_modified
-          next if (!self.last_read_at.nil? and published < self.last_read_at) # or published < (Gaps.max+10).days.ago \
+          next if (!self.last_read_at.nil? and published < self.last_read_at) or published < (Gaps.max+10).days.ago \
 
         items[i.url] = { :created_at=>published, :title=>i.title }
 
